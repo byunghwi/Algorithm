c1f1c694b79e2e9091713505e10d61a5a72589c6
byunghwi <gpdldy91@gmail.com>
2023-10-10 17:40:26 +0900

    Update 2222-number-of-ways-to-select-buildings.js

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.js
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.js
@@ -4,7 +4,7 @@
  */
 var numberOfWays = function(s) {
     //가능한 조합 010, 101 이므로
-    // 현재 인덱스의 숫자 이전의 반대되는 숫자들 갯수 * 현재 인덱스의 숫자 이후의 반대되는 숫자들 갯수
+    // 현재 인덱스의 숫자 이전의 반대되는 숫자들 갯수(ex 현재 1이라면 현재 인덱스 이전의 0인 숫자의 갯수) * 현재 인덱스의 숫자 이후의 반대되는 숫자들 갯수
 
     var totalZeros = 0; // 전체 0의 갯수
     var totalOnes = 0;  // 전체 1의 갯수
@@ -26,10 +26,10 @@ var numberOfWays = function(s) {
     leftOnes =  (arrChar[0] == '1') ? 1 : 0;
 
     for(var j=1; j<arrChar.length; j++) {
-        if(arrChar[j] == 0) { //현재 인덱스 값 0일 때,
+        if(arrChar[j] == 0) { //현재 인덱스 값 0일 때, 좌측의 값이 1인 인덱스들 갯수 * 우측의 값이 1인 인덱스들 갯수
             result = result + (leftOnes * (totalOnes - leftOnes));
             leftZeros++;
-        } else { // 현재 인덱스 값 1일 때,
+        } else { // 현재 인덱스 값 1일 때, 좌측의 값이 0인 인덱스들 갯수 * 우측의 값이 0인 인덱스들 갯수
             result = result + (leftZeros * (totalZeros - leftZeros));
             leftOnes++;
         }
